f903116f2ed8033cd7141e5434a511fb9ab2e47f
Derek White <derekwhite@gmail.com>
2010-10-05 11:15:00 -0500

    Created common sql server commands file

---
+++ common_mssql.sql
@@ -0,0 +1,22 @@
+/* Create a new Table if it doesn't exist */
+IF OBJECT_ID('tablename') IS NULL
+BEGIN
+    print 'Create tablename table'
+    CREATE TABLE tablename
+    (
+        id int identity NOT NULL,
+        name nvarchar(50) NOT NULL,
+        createdBy int NOT NULL,
+        createddate datetime NOT NULL
+    )
+END
+GO
+
+
+/* Add a new column to a table if it doesn't exist'
+IF NOT EXISTS (SELECT * FROM syscolumns WHERE id=object_id('tablename') AND name='columnname')
+BEGIN
+    ALTER TABLE tablename
+        Add columnname int NOT NULL DEFAULT 0
+END
+GO
