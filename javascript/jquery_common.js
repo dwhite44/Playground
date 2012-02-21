70be91ce5629bbc83383d642ee9b83e1fe034acb
Derek White <derekwhite@gmail.com>
2012-02-20 22:54:03 -0600

    Add populateSelectListFromAjax method to common javascript

--- javascript/jquery_common.js
+++ javascript/jquery_common.js
@@ -55,6 +55,57 @@
               url = url.substr(0, url.length - 1);
 
             return url;
+        },
+        populateSelectListFromAjax: function($select_ctrl, url, value_field, text_field, not_found_text, exclude_default_option) 
+        {
+            $select_ctrl.html('');
+            $select_ctrl.val('Loading..')
+            $.ajax(
+            {
+                url: url,
+                dataType: "json",
+                success: function (data, status, xhr)
+                {
+                    $select_ctrl.html("");
+
+                    if (data.length == 0)
+                    {
+                        if (typeof not_found_text == 'undefined' || not_found_text == null || not_found_text.length == 0)
+                            not_found_text = "No items found";
+
+                        $select_ctrl.append($.create_select_item("", not_found_text));
+                    }
+                    else
+                    {
+                        if (typeof exclude_default_option == 'undefined' || exclude_default_option == null || exclude_default_option == false)
+                            $select_ctrl.append($.create_select_item("", ""));
+
+                        $.each(data, function (index, item)
+                        {
+                            $select_ctrl.append($.create_select_item(item[value_field], item[text_field]));
+                        });
+                    }
+
+                    $select_ctrl.trigger('ajax:success', [data, status, xhr]);
+                },
+                error: function (xhr, status, error)
+                {
+                    //alert("Error: " + status);
+                    $select_ctrl.html('');
+                    $select_ctrl.append($.create_select_item('', 'Error...'));
+                    $select_ctrl.trigger("ajax:failure", [xhr, status, error]);
+
+                },
+                complete: function (xhr)
+                {
+                    $select_ctrl.removeClass("ui-autocomplete-loading");
+                    $select_ctrl.trigger("ajax:complete", xhr);
+                }
+            });
+        },
+        create_select_item: function(value, text)
+        {
+          return $("<option></option>").val(value).html(text);
         }
     });
 })(jQuery);
